6febbe2f08ccd6f2f4a683fea8956a6d695d74aa
Noah MacCallum <noahmacca@gmail.com>
2024-01-25 18:58:25 -0800

    Update image rendering to use smaller heatmap

--- app/lib/ui/ImageWithHeatmap.tsx
+++ app/lib/ui/ImageWithHeatmap.tsx
@@ -13,10 +13,10 @@ export default function ImageWithHeatmap({
         className="block w-full rounded"
       />
       <div className="opacity-50 hover:opacity-0 transition-opacity duration-150">
-        <div className="bg-red-600 absolute top-0 left-0 w-full h-full"></div>
         <img
           src={`data:image/jpeg;base64,${heatmapData}`}
-          className="absolute top-0 left-0 mix-blend-multiply w-full rounded"
+          style={{ imageRendering: "pixelated" }}
+          className="absolute top-0 left-0 w-full rounded"
           alt="Activation heatmap"
         />
       </div>
