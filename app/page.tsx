889f734ead7f441be018e0ef190f5be770c8eff2
Noah MacCallum <noahmacca@gmail.com>
2024-01-28 11:18:14 -0800

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col p-24 w-xl">
-      <div className="text-4xl font-semibold mt-12 mb-4">Welcome!</div>
+      <div className="text-4xl font-semibold mt-12 mb-2">Mindreader</div>
+      <div>
+        This is a small mechanistic interpretability project to try and
+        understand what the neurons inside vision transformers are actually
+        learning.
+      </div>
+      <Link
+        className="text-blue-600 hover:text-blue-800 visited:text-purple-600 mt-4"
+        href="/neuron"
+      >
+        Top activating neurons
+      </Link>
     </main>
   );
 }
