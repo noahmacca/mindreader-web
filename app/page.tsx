b0ac06d07a20df7438215d28b783c2edb455a692
Noah MacCallum <noahmacca@gmail.com>
2024-01-28 14:36:46 -0800

    Update copy on cover page

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,23 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-24 w-xl">
+    <main className="flex min-h-screen flex-col p-24 w-xl max-w-4xl mx-auto">
       <div className="text-4xl font-semibold mt-12 mb-2">Mindreader</div>
       <div>
-        This is a small mechanistic interpretability project to try and
-        understand what the neurons inside vision transformers are actually
-        learning.
+        Can we interpret what's happening inside of vision transformers? This is
+        a simple project that explores the concepts being learned by individual
+        neurons in TinyCLIP. understand what the neurons inside vision
+        transformers are actually learning.
       </div>
-      <Link
-        className="text-blue-600 hover:text-blue-800 visited:text-purple-600 mt-4"
-        href="/neuron"
-      >
-        Top activating neurons
-      </Link>
+      <div className="mt-8">
+        <Link
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          href="/neuron"
+        >
+          Explore TinyCLIP
+        </Link>
+      </div>
+      <div className="mt-4 italic text-gray-400">More coming soon...</div>
     </main>
   );
 }
